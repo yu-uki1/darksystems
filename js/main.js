6a02ca52b271734b2a28d71cca85caa74cda8fc0
Yuuki <145976791+yu-uki1@users.noreply.github.com>
2026-09-14 10:09:14 +0200

    Add mobile navigation styles for main navigation

---
+++ js/main.js
@@ -0,0 +1,23 @@
+@media (max-width: 800px) {
+
+    .main-nav.mobile-nav-open {
+        position: absolute;
+
+        top: 62px;
+        left: 0;
+        right: 0;
+
+        display: flex;
+        flex-direction: column;
+
+        padding: 25px;
+
+        background: var(--bg);
+        border-bottom: 1px solid var(--border);
+
+        box-shadow: var(--shadow);
+
+        gap: 20px;
+    }
+
+}
